04a3bc1732be4ef2beba8de4e4879ce1e1ed2e66
ian knauer <ian.knauer@gmail.com>
2018-06-07 14:23:19 -0700

    updated style for primary page

--- app/tailwind/config/fonts.js
+++ app/tailwind/config/fonts.js
@@ -18,18 +18,8 @@
 
 export default {
   'sans': [
-    'system-ui',
-    'BlinkMacSystemFont',
-    '-apple-system',
-    'Segoe UI',
-    'Roboto',
-    'Oxygen',
-    'Ubuntu',
-    'Cantarell',
-    'Fira Sans',
-    'Droid Sans',
-    'Helvetica Neue',
-    'sans-serif',
+    'open sans',
+
   ],
   'serif': [
     'Constantia',
