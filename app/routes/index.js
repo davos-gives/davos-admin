6e46922775e23b3a73c1f2ab2d991277472d31d7
ian knauer <ian.knauer@gmail.com>
2018-07-30 10:40:45 -0700

    updated route for alice (demo)

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default Route.extend({
   model() {
-    return this.store.loadRecord('donor', 'ianknauer', {
+    return this.store.loadRecord('donor', 'alicechambers', {
      include: 'relationships,relationships.recurrings,relationships.recurrings.campaign,relationships.recurrings.campaign.organization'
     });
   }
